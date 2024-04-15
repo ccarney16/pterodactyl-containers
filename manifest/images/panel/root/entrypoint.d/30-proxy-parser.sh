a4b991413f810746ad60e318013e0c24ec9a6073
Cameron Carney <ccarney16@live.com>
2024-04-15 12:02:39 -0400

    prevent wildcard from escaping string

--- manifest/images/panel/root/entrypoint.d/30-proxy-parser.sh
+++ manifest/images/panel/root/entrypoint.d/30-proxy-parser.sh
@@ -6,7 +6,7 @@ CADDY_TRUSTED_PROXIES=""
 
 if ! [[ -z "$TRUSTED_PROXIES" ]]; then
     printf "[proxy-parser] Enabling trusted_proxies directive for web server.\n"
-    export CADDY_TRUSTED_PROXIES="trusted_proxies $(echo $TRUSTED_PROXIES | sed 's/,/ /g')"
+    export CADDY_TRUSTED_PROXIES="trusted_proxies $(echo "$TRUSTED_PROXIES" | sed 's/,/ /g')"
 
     # Resets TRUSTED_PROXIES for pterodactyl to accept any web proxy (caddy will handle which proxy is actually trusted)
     export TRUSTED_PROXIES="**"
